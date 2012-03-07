220c44615ef76ce4b7fae116325c11e1b9ac57a8
steigert@gmail.com <steigert@gmail.com@aaa5c38f-40d0-5178-f449-cb61844b74a3>
2012-03-07 02:32:57 +0000

    Tag for the post #5

---
+++ tags/post-20120306/tyrian-game/src/com/blogspot/steigert/tyrian/domain/Profile.java
@@ -0,0 +1,142 @@
+package com.blogspot.steigert.tyrian.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.Json.Serializable;
+import com.badlogic.gdx.utils.OrderedMap;
+
+/**
+ * The player's profile.
+ */
+public class Profile
+    implements
+        Serializable
+{
+    private int currentLevelId;
+    private int credits;
+    private Map<Integer,Integer> highScores;
+    private Ship ship;
+
+    public Profile()
+    {
+        highScores = new HashMap<Integer,Integer>();
+    }
+
+    /**
+     * Retrieves the ID of the next playable level.
+     */
+    public int getCurrentLevelId()
+    {
+        return currentLevelId;
+    }
+
+    /**
+     * Retrieves the high scores for each level (Level-ID -> High score).
+     */
+    public Map<Integer,Integer> getHighScores()
+    {
+        return highScores;
+    }
+
+    /**
+     * Gets the current high score for the given level.
+     */
+    public int getHighScore(
+        int levelId )
+    {
+        if( highScores == null ) return 0;
+        Integer highScore = highScores.get( levelId );
+        return ( highScore == null ? 0 : highScore );
+    }
+
+    /**
+     * Notifies the score on the given level. Returns <code>true</code> if its a
+     * high score.
+     */
+    public boolean notifyScore(
+        int levelId,
+        int score )
+    {
+        if( score > getHighScore( levelId ) ) {
+            highScores.put( levelId, score );
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Retrieves the amount of credits the player has.
+     */
+    public int getCredits()
+    {
+        return credits;
+    }
+
+    /**
+     * Retrieves the current ship configuration.
+     */
+    public Ship getShip()
+    {
+        return ship;
+    }
+
+    /**
+     * Checks whether the given item can be bought.
+     */
+    public boolean canBuy(
+        Item item )
+    {
+        if( ship.contains( item ) ) {
+            return false;
+        }
+        if( item.getPrice() > credits ) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Buys the given item.
+     */
+    public void buy(
+        Item item )
+    {
+        if( canBuy( item ) ) {
+            credits -= item.getPrice();
+            ship.install( item );
+        }
+    }
+
+    // Serializable implementation
+
+    @SuppressWarnings( "unchecked" )
+    @Override
+    public void read(
+        Json json,
+        OrderedMap<String,Object> jsonData )
+    {
+        currentLevelId = json.readValue( "currentLevelId", Integer.class, jsonData );
+        credits = json.readValue( "credits", Integer.class, jsonData );
+
+        // libgdx handles the keys of JSON formatted HashMaps as Strings, but we
+        // want it to be an integer instead (levelId)
+        Map<String,Integer> highScores = json.readValue( "highScores", HashMap.class,
+            Integer.class, jsonData );
+        for( String levelIdAsString : highScores.keySet() ) {
+            int levelId = Integer.valueOf( levelIdAsString );
+            Integer highScore = highScores.get( levelIdAsString );
+            this.highScores.put( levelId, highScore );
+        }
+    }
+
+    @Override
+    public void write(
+        Json json )
+    {
+        json.writeValue( "currentLevelId", currentLevelId );
+        json.writeValue( "credits", credits );
+        json.writeValue( "highScores", highScores );
+    }
+}
