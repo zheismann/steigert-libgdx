e0a232ac5fb8e687d3ce629f020aad5902eaf553
steigert@gmail.com <steigert@gmail.com@aaa5c38f-40d0-5178-f449-cb61844b74a3>
2012-03-08 02:40:55 +0000

    Tag for the post #6

---
+++ tags/post-20120307/tyrian-game/src/com/blogspot/steigert/tyrian/screens/MenuScreen.java
@@ -0,0 +1,99 @@
+package com.blogspot.steigert.tyrian.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.tablelayout.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.tablelayout.TableLayout;
+import com.blogspot.steigert.tyrian.Tyrian;
+
+public class MenuScreen
+    extends
+        AbstractScreen
+{
+    private Table table;
+
+    public MenuScreen(
+        Tyrian game )
+    {
+        super( game );
+    }
+
+    @Override
+    public void show()
+    {
+        super.show();
+
+        // retrieve the custom skin for our 2D widgets
+        Skin skin = super.getSkin();
+
+        // create the table actor and add it to the stage
+        table = new Table( skin );
+        stage.addActor( table );
+
+        // retrieve the table's layout
+        TableLayout layout = table.getTableLayout();
+
+        // register the button "start game"
+        TextButton startGameButton = new TextButton( "Start game", skin );
+        startGameButton.setClickListener( new ClickListener() {
+            @Override
+            public void click(
+                Actor actor,
+                float x,
+                float y )
+            {
+                game.setScreen( game.getStartGameScreen() );
+            }
+        } );
+        layout.register( "startGameButton", startGameButton );
+
+        // register the button "options"
+        TextButton optionsButton = new TextButton( "Options", skin );
+        optionsButton.setClickListener( new ClickListener() {
+            @Override
+            public void click(
+                Actor actor,
+                float x,
+                float y )
+            {
+                game.setScreen( game.getOptionsScreen() );
+            }
+        } );
+        layout.register( "optionsButton", optionsButton );
+
+        // register the button "high scores"
+        TextButton highScoresButton = new TextButton( "High Scores", skin );
+        highScoresButton.setClickListener( new ClickListener() {
+            @Override
+            public void click(
+                Actor actor,
+                float x,
+                float y )
+            {
+                game.setScreen( game.getHighScoresScreen() );
+            }
+        } );
+        layout.register( "highScoresButton", highScoresButton );
+
+        // finally, parse the layout descriptor
+        layout.parse( Gdx.files.internal( "layout-descriptors/menu-screen.txt" ).readString() );
+    }
+
+    @Override
+    public void resize(
+        int width,
+        int height )
+    {
+        super.resize( width, height );
+
+        // resize the table
+        table.width = width;
+        table.height = height;
+
+        // we need a complete redraw
+        table.invalidateHierarchy();
+    }
+}
---
+++ tags/post-20120307/tyrian-game/src/com/blogspot/steigert/tyrian/screens/SplashScreen.java
@@ -0,0 +1,94 @@
+package com.blogspot.steigert.tyrian.screens;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.OnActionCompleted;
+import com.badlogic.gdx.scenes.scene2d.actions.Delay;
+import com.badlogic.gdx.scenes.scene2d.actions.FadeIn;
+import com.badlogic.gdx.scenes.scene2d.actions.FadeOut;
+import com.badlogic.gdx.scenes.scene2d.actions.Sequence;
+import com.badlogic.gdx.scenes.scene2d.ui.Align;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Scaling;
+import com.blogspot.steigert.tyrian.Tyrian;
+
+/**
+ * Shows a splash image and moves on to the next screen.
+ */
+public class SplashScreen
+    extends
+        AbstractScreen
+{
+    private Texture splashTexture;
+    private Image splashImage;
+
+    public SplashScreen(
+        Tyrian game )
+    {
+        super( game );
+    }
+
+    @Override
+    public void show()
+    {
+        super.show();
+
+        // load the texture with the splash image
+        splashTexture = new Texture( "image-atlases/splash.png" );
+
+        // set the linear texture filter to improve the image stretching
+        splashTexture.setFilter( TextureFilter.Linear, TextureFilter.Linear );
+
+        // in the image atlas, our splash image begins at (0,0) of the
+        // upper-left corner and has a dimension of 512x301
+        TextureRegion splashRegion = new TextureRegion( splashTexture, 0, 0, 512, 301 );
+
+        // here we create the splash image actor; its size is set when the
+        // resize() method gets called
+        splashImage = new Image( splashRegion, Scaling.stretch, Align.BOTTOM | Align.LEFT );
+
+        // this is needed for the fade-in effect to work correctly; we're just
+        // making the image completely transparent
+        splashImage.color.a = 0f;
+
+        // configure the fade-in/out effect on the splash image
+        Sequence actions = Sequence.$( FadeIn.$( 0.75f ), Delay.$( FadeOut.$( 0.75f ), 1.75f ) );
+        actions.setCompletionListener( new OnActionCompleted() {
+            @Override
+            public void completed(
+                Action action )
+            {
+                // when the image is faded out, move on to the next screen
+                game.setScreen( game.getMenuScreen() );
+            }
+        } );
+        splashImage.action( actions );
+
+        // and finally we add the actor to the stage
+        stage.addActor( splashImage );
+    }
+
+    @Override
+    public void resize(
+        int width,
+        int height )
+    {
+        super.resize( width, height );
+
+        // resize the splash image
+        splashImage.width = width;
+        splashImage.height = height;
+
+        // we need a complete redraw
+        splashImage.invalidateHierarchy();
+    }
+
+    @Override
+    public void dispose()
+    {
+        super.dispose();
+        splashTexture.dispose();
+    }
+}
