44650f30fb8692578eb4dcb9e32aa6d2e7ba4a24
steigert@gmail.com <steigert@gmail.com@aaa5c38f-40d0-5178-f449-cb61844b74a3>
2012-06-24 17:48:00 +0000

    Tag for the post #12

---
+++ tags/post-20120625/tyrian-game/src/com/blogspot/steigert/tyrian/domain/Profile.java
@@ -0,0 +1,175 @@
+package com.blogspot.steigert.tyrian.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.Json.Serializable;
+import com.badlogic.gdx.utils.OrderedMap;
+import com.blogspot.steigert.tyrian.Tyrian;
+import com.blogspot.steigert.tyrian.services.ProfileManager;
+import com.blogspot.steigert.tyrian.utils.TextUtils;
+
+/**
+ * The player's profile.
+ * <p>
+ * This class is used to store the game progress, and is persisted to the file
+ * system when the game exists.
+ * 
+ * @see ProfileManager
+ */
+public class Profile
+    implements
+        Serializable
+{
+    private int currentLevelId;
+    private int credits;
+    private Map<Integer,Integer> highScores;
+    private Ship ship;
+
+    public Profile()
+    {
+        credits = 1000;
+        highScores = new HashMap<Integer,Integer>();
+        ship = new Ship();
+        ship.install( ShipModel.USP_TALON );
+        ship.install( FrontGun.PULSE_CANNON );
+        ship.install( Shield.SIF );
+    }
+
+    /**
+     * Retrieves the ID of the next playable level.
+     */
+    public int getCurrentLevelId()
+    {
+        return currentLevelId;
+    }
+
+    /**
+     * Retrieves the high scores for each level (Level-ID -> High score).
+     */
+    public Map<Integer,Integer> getHighScores()
+    {
+        return highScores;
+    }
+
+    /**
+     * Gets the current high score for the given level.
+     */
+    public int getHighScore(
+        int levelId )
+    {
+        if( highScores == null ) return 0;
+        Integer highScore = highScores.get( levelId );
+        return ( highScore == null ? 0 : highScore );
+    }
+
+    /**
+     * Notifies the score on the given level. Returns <code>true</code> if its a
+     * high score.
+     */
+    public boolean notifyScore(
+        int levelId,
+        int score )
+    {
+        if( score > getHighScore( levelId ) ) {
+            highScores.put( levelId, score );
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Retrieves the amount of credits the player has.
+     */
+    public int getCredits()
+    {
+        return credits;
+    }
+
+    /**
+     * Retrieves the amount of credits as text.
+     */
+    public String getCreditsAsText()
+    {
+        return TextUtils.creditStyle( credits );
+    }
+
+    /**
+     * Retrieves the current ship configuration.
+     */
+    public Ship getShip()
+    {
+        return ship;
+    }
+
+    /**
+     * Checks whether the given item can be bought.
+     */
+    public boolean canBuy(
+        Item item )
+    {
+        if( ship.contains( item ) ) {
+            return false;
+        }
+        if( item.getPrice() > credits ) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Buys the given item.
+     */
+    public boolean buy(
+        Item item )
+    {
+        if( canBuy( item ) ) {
+            Gdx.app.log( Tyrian.LOG, "Buying item: " + item );
+            ship.install( item );
+            credits -= item.getPrice();
+            Gdx.app.log( Tyrian.LOG, "Credits available: " + credits );
+            return true;
+        } else {
+            Gdx.app.log( Tyrian.LOG, "No credits to buy item: " + item );
+            return false;
+        }
+    }
+
+    // Serializable implementation
+
+    @SuppressWarnings( "unchecked" )
+    @Override
+    public void read(
+        Json json,
+        OrderedMap<String,Object> jsonData )
+    {
+        // read the some basic properties
+        currentLevelId = json.readValue( "currentLevelId", Integer.class, jsonData );
+        credits = json.readValue( "credits", Integer.class, jsonData );
+
+        // libgdx handles the keys of JSON formatted HashMaps as Strings, but we
+        // want it to be an integer instead (levelId)
+        Map<String,Integer> highScores = json.readValue( "highScores", HashMap.class,
+            Integer.class, jsonData );
+        for( String levelIdAsString : highScores.keySet() ) {
+            int levelId = Integer.valueOf( levelIdAsString );
+            Integer highScore = highScores.get( levelIdAsString );
+            this.highScores.put( levelId, highScore );
+        }
+
+        // finally, read the ship
+        ship = json.readValue( "ship", Ship.class, jsonData );
+    }
+
+    @Override
+    public void write(
+        Json json )
+    {
+        json.writeValue( "currentLevelId", currentLevelId );
+        json.writeValue( "credits", credits );
+        json.writeValue( "highScores", highScores );
+        json.writeValue( "ship", ship );
+    }
+}
---
+++ tags/post-20120625/tyrian-game/src/com/blogspot/steigert/tyrian/screens/HighScoresScreen.java
@@ -0,0 +1,79 @@
+package com.blogspot.steigert.tyrian.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.tablelayout.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.tablelayout.TableLayout;
+import com.blogspot.steigert.tyrian.Tyrian;
+import com.blogspot.steigert.tyrian.domain.Profile;
+import com.blogspot.steigert.tyrian.services.SoundManager.TyrianSound;
+
+/**
+ * A simple high scores screen.
+ */
+public class HighScoresScreen
+    extends
+        AbstractScreen
+{
+    private Table table;
+
+    public HighScoresScreen(
+        Tyrian game )
+    {
+        super( game );
+    }
+
+    @Override
+    public void show()
+    {
+        super.show();
+
+        // retrieve the custom skin for our 2D widgets
+        Skin skin = super.getSkin();
+
+        // create the table actor and add it to the stage
+        table = new Table( skin );
+        table.width = stage.width();
+        table.height = stage.height();
+        stage.addActor( table );
+
+        // retrieve the table's layout
+        TableLayout layout = table.getTableLayout();
+        Profile profile = game.getProfileManager().retrieveProfile();
+
+        // create the labels widgets
+        String level1Highscore = String.valueOf( profile.getHighScore( 0 ) );
+        Label episode1HighScore = new Label( level1Highscore, skin );
+        layout.register( "episode1HighScore", episode1HighScore );
+
+        String level2Highscore = String.valueOf( profile.getHighScore( 1 ) );
+        Label episode2HighScore = new Label( level2Highscore, skin );
+        layout.register( "episode2HighScore", episode2HighScore );
+
+        String level3Highscore = String.valueOf( profile.getHighScore( 2 ) );
+        Label episode3HighScore = new Label( level3Highscore, skin );
+        layout.register( "episode3HighScore", episode3HighScore );
+
+        // register the back button
+        TextButton backButton = new TextButton( "Back to main menu", skin );
+        backButton.setClickListener( new ClickListener() {
+            @Override
+            public void click(
+                Actor actor,
+                float x,
+                float y )
+            {
+                game.getSoundManager().play( TyrianSound.CLICK );
+                game.setScreen( new MenuScreen( game ) );
+            }
+        } );
+        layout.register( "backButton", backButton );
+
+        // finally, parse the layout descriptor
+        layout.parse( Gdx.files.internal( "layout-descriptors/high-scores-screen.txt" ).readString() );
+    }
+}
